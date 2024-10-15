5887a92f517f072d940e4a092676ccbaa489bf53
tacticalduck1237 <egornikulin17@gmail.com>
2024-10-15 22:51:18 +0300

    updated 3_2

--- Programming Fundamentals and Data Analysis with Python/fall 2024/03/3_2.py
+++ Programming Fundamentals and Data Analysis with Python/fall 2024/03/3_2.py
@@ -1,6 +1,13 @@
 s = open('input.txt')
 c = s.read()
-filtered_words = [word for word in c.split() if 'ё' in word]
+import string
+punctuations = string.punctuation
+c1=""
+for char in c:
+    if char not in punctuations:
+        c1 += char
+c1=c1.split()
+filtered_words = [word for word in c1 if 'ё' in word]
 d = '\n'.join(filtered_words)
 print(d)
 s.close()
