a22e9a47c2d1019628db287910203d95b0b7808d
tacticalduck1237 <egornikulin17@gmail.com>
2024-10-27 22:34:39 +0300

    added P# in hometask 3

--- Applied Machine Learning with Python/Fall 2024/03/P3.py
+++ Applied Machine Learning with Python/Fall 2024/03/P3.py
@@ -1,21 +1,18 @@
 import numpy as np
-
 class KNN_classifier:
     def __init__(self, n_neighbors: int, **kwargs):
         self.K = n_neighbors
-
-    def fit(self, x: np.array, y: np.array):
-        self.x_train = x
+    def fit(self, X: np.array, y: np.array):
+        self.X_train = X
         self.y_train = y
 
-    def predict(self, x: np.array):
+    def predict(self, X: np.array):
         predictions = []
-        for x_test in x:
-            distances = np.linalg.norm(self.x_train - x_test, axis=1)
+        for X_test in X:
+            distances = np.linalg.norm(self.X_train - X_test, axis=1)
             k_indices = np.argsort(distances)[:self.K]
             k_nearest_labels = self.y_train[k_indices]
             most_common = np.bincount(k_nearest_labels).argmax()
             predictions.append(most_common)
         predictions = np.array(predictions)
         return predictions
-        
