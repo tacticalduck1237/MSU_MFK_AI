ee03e1bebdd0b1fbd2cad82d2d41425b7db09df0
tacticalduck1237 <egornikulin17@gmail.com>
2024-10-27 20:11:14 +0300

    p2 and p3

--- Applied Machine Learning with Python/Fall 2024/03/P2.py
+++ Applied Machine Learning with Python/Fall 2024/03/P2.py
@@ -1,4 +1,41 @@
 #ссылка на ноутбук https://colab.research.google.com/drive/1ZD6UuuUmUKIyF0lTES0-nL6RpCZD8htf?usp=sharing
 
-#У МЕНЯ РЕШЕНО НЕ ВЕРНО
+#ПРОРЕШАННЫЙ НОУТБУК
+with open('input.txt') as file:
+    for s in file:
+        g = s.split()
+import sklearn
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+random_seed = 4238
 
+np.random.seed(random_seed)
+n_splits = 3
+
+from sklearn.datasets import load_iris
+
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+k_values = range(1, 51)
+mean_scores = []
+
+for k in k_values:
+    knn = KNeighborsClassifier(n_neighbors=k, p=1)
+    scores = cross_val_score(knn, X_train, y_train, cv=5, scoring="accuracy")
+    mean_scores.append(scores.mean())
+    pass
+plt.figure(figsize=(10, 7))
+plt.plot(k_values, mean_scores)
+plt.title('KNN Cross Validation Scores')
+plt.xlabel('Number of Neighbors (K)')
+plt.ylabel('Average Score')
+plt.grid(True)
+plt.show()
+
+optimal_k = k_values[np.argmax(mean_scores)]
+print(optimal_k)
+#Ответ:евклидово расстояние
---
+++ Applied Machine Learning with Python/Fall 2024/03/P3.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class KNN_classifier:
+    def __init__(self, n_neighbors: int, **kwargs):
+        self.K = n_neighbors
+
+    def fit(self, x: np.array, y: np.array):
+        self.x_train = x
+        self.y_train = y
+
+    def predict(self, x: np.array):
+        predictions = []
+        for x_test in x:
+            distances = np.linalg.norm(self.x_train - x_test, axis=1)
+            k_indices = np.argsort(distances)[:self.K]
+            k_nearest_labels = self.y_train[k_indices]
+            most_common = np.bincount(k_nearest_labels).argmax()
+            predictions.append(most_common)
+        predictions = np.array(predictions)
+        return predictions
