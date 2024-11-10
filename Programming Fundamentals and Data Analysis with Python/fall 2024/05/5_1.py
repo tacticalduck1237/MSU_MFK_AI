b677db9010b8558391964f069e64fb43cdd6b7d0
tacticalduck1237 <egornikulin17@gmail.com>
2024-11-10 14:14:57 +0300

    5_1 py

--- Programming Fundamentals and Data Analysis with Python/fall 2024/05/5_1.py
+++ Programming Fundamentals and Data Analysis with Python/fall 2024/05/5_1.py
@@ -1,1 +1,6 @@
-sss=open('input.txt')
+start, stop, step = map(int, input().split())
+n = list(map(int, input().split()))
+progression = range(start, stop, step)
+for i in progression:
+    n[i] = i
+print(n)
